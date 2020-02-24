086883c722137f4be7c15f3dc0ed0d9d245066d8
lobnamt <lobna.mtet001@umb.edu>
2020-02-23 21:23:05 -0500

    Add canvas and bootstrap and jquery

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,47 @@
-var video = document.querySelector("#videoElement");
+var video;
+var canvas;
+var context;
+var initialized = false;
 
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  init();
+});
+
+function draw(v,c,w,h){
+  c.drawImage(v,0,0,w,h);
+  setTimeout(draw,20,v,c,w,h);
+
+}
+
+function init(){
+  video = document.querySelector("#videoElement");
+  canvas = document.getElementById('canvas');
+  context = canvas.getContext('2d');
+  initialized = true;
+}
+
+function start(){
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {
       video.srcObject = stream;
+      if(initialized){
+        draw(video,context,canvas.width,canvas.height);
+      }else{
+        alert("Camera is not ready, try again!");
+      }
     })
     .catch(function (err0r) {
       console.log("Something went wrong!");
     });
+}
+}
+
+function stop() {
+  video.srcObject = null;
+}
+
+function screenshot(){
+
 }
